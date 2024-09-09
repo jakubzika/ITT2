f4a1ba95d6086d329b790996d44adb58d6c22427
Kysel235 <mikikostak235@gmail.com>
2024-09-09 21:34:13 +0200

    audio_controller update

--- portrait/midi_map.py
+++ portrait/midi_map.py
@@ -3,6 +3,11 @@ from midi.midi import Midi
 midi_controller = Midi()
 
 #%%
+starting_civ_idx = 80
+for i in range(starting_civ_idx,starting_civ_idx+6):
+    midi_controller.send_control(i, 0)
+    
+#%%
 import rtmidi
 from rtmidi.midiconstants import CONTROL_CHANGE
 midiout = rtmidi.MidiOut()
@@ -30,5 +35,10 @@ for i in range(80,80+6):
         midi_controller.send_control(i, 0)
 
 #%%
-for i in range(90,90+12):
-    midi_controller.send_control(i, 60)
+starting_nat_idx = 90
+for i in range(starting_nat_idx,starting_nat_idx+12):
+    if True:#i == starting_idx+2:
+        midi_controller.send_control(i, 120)
+    else:
+        midi_controller.send_control(i, 0)
+
